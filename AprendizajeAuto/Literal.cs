35eae71b451704e16087247514e1922277a0fd61
Jesus Perez <jesus21hlv@gmail.com>
2016-08-09 11:21:37 +0200

    Estructura nueva version

---
+++ AprendizajeAuto/Literal.cs
@@ -0,0 +1,30 @@
+﻿namespace AprendizajeAuto
+{
+    class Literal
+    {
+        private string nombre;
+        private int nAtributos;
+        private string[] atributos;
+
+        public string Nombre
+        {
+            get { return nombre; }
+        }
+        public int nAtt
+        {
+            get { return nAtributos; }
+        }
+        public string[] Atributos
+        {
+            get { return atributos; }
+            set { if(value.Length == nAtributos) atributos = value; }
+        }
+
+        public Literal(string _nombre, int _nAtt)
+        {
+            nombre = _nombre;
+            nAtributos = _nAtt;
+
+        }
+    }
+}
